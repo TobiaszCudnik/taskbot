3b4327e3c698042f89113a9432f31650e71f0303
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-02-19 18:46:07 -0500

    label filters sync

--- src/sync/sync-label-filter.ts
+++ src/sync/sync-label-filter.ts
@@ -1,42 +1,40 @@
-import { Sync, SyncState } from './sync'
-import {ILabelFilter} from "../types";
-import {debug} from "util";
-import RootSync from "./root";
+import { Sync, SyncState } from './sync';
+import { ILabelFilter } from '../types';
+import { debug } from 'util';
+import RootSync from './root';
+import * as clone from 'deepcopy';
 
 export class State extends SyncState {
   constructor(target: Sync) {
-    super(target)
-    this.registerAll()
+    super(target);
+    this.registerAll();
   }
 }
 
 export default class GmailLabelFilterSync extends Sync {
+  log = debug(this.state.id(true));
 
-  log = debug(this.state.id(true))
-
-  constructor(
-    public config: ILabelFilter,
-    public root: RootSync
-  ) {
-    super(config)
+  constructor(public config: ILabelFilter, public root: RootSync) {
+    super(config);
   }
 
   Reading_state() {
-    this.state.add('ReadingDone')
+    this.state.add('ReadingDone');
   }
 
   getState() {
-    return new State(this).id('label-filter: '+this.config.name)
+    return new State(this).id('label-filter: ' + this.config.name);
   }
 
   merge(): number[] {
-    let count = 0
+    let count = 0;
     for (const r of this.root.data.where(this.config.db_query).data()) {
-      const {add, remove} = this.config
-      this.applyLabels(r, {add, remove})
-      this.root.data.update(r)
-      count++
+      const before = clone(r);
+      const { add, remove } = this.config;
+      this.applyLabels(r, { add, remove });
+      this.compareRecord(before, r);
+      count++;
     }
-    return count ? [count] : []
+    return count ? [count] : [];
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { DBRecord } from './sync/root'
+import { DBRecord } from './sync/root';
 
 // export interface ILabelDefaults {
 //   email_unmatched?: string[]
@@ -10,55 +10,55 @@ import { DBRecord } from './sync/root'
 // }
 
 export interface ILabelFilter {
-  name: string,
-  db_query: (r: DBRecord) => boolean
-  add: string[]
-  remove: string[]
+  name: string;
+  db_query: (r: DBRecord) => boolean;
+  add: string[];
+  remove: string[];
 }
 
 export interface IListConfig {
-  name: string
-  gmail_query: string
-  db_query: (record: DBRecord) => boolean
+  name: string;
+  gmail_query: string;
+  db_query: (record: DBRecord) => boolean;
   enter: {
-    add: string[]
-    remove: string[]
-  }
+    add: string[];
+    remove: string[];
+  };
   exit: {
-    add: string[]
-    remove: string[]
-  }
+    add: string[];
+    remove: string[];
+  };
 }
 
 export type IConfig = {
-  debug: boolean
+  debug: boolean;
   google: {
-    scopes: string[]
-  }
-  client_id: string
-  client_secret: string
-  access_token: string
-  refresh_token: string
-  gmail_username: string
-  gmail_password: string
-  gmail_host: string
-  redirect_url: string
+    scopes: string[];
+  };
+  client_id: string;
+  client_secret: string;
+  access_token: string;
+  refresh_token: string;
+  gmail_username: string;
+  gmail_password: string;
+  gmail_host: string;
+  redirect_url: string;
   text_labels: {
-    symbol: string
-    shortcut?: string
-    label?: string
-    prefix?: string
-    create?: boolean
-  }[]
-  label_filters: ILabelFilter[]
-  status_labels: string[]
-  sync_frequency: number
-  lists: IListConfig[]
+    symbol: string;
+    shortcut?: string;
+    label?: string;
+    prefix?: string;
+    create?: boolean;
+  }[];
+  label_filters: ILabelFilter[];
+  status_labels: string[];
+  sync_frequency: number;
+  lists: IListConfig[];
   // TODO this could be dynamic?
-  status_map: { [shortcut: string]: string }
+  status_map: { [shortcut: string]: string };
   // lists_labels_in_title: LabelsInTitles
   // lists_defaults: ILabelDefaults,
-}
+};
 
 // TODO
 export enum LabelsInTitles {
@@ -67,4 +67,4 @@ export enum LabelsInTitles {
   C
 }
 
-export type TRawEmail = string
+export type TRawEmail = string;
