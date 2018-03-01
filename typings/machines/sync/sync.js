4a0bbb0427ab48c8d2911cad63c7a377d1c3b76b
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-03-01 14:03:55 -0500

    cleanup

--- typings/machines/sync/sync.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-const asyncmachine_1 = require('asyncmachine')
-exports.AsyncMachine = asyncmachine_1.default
-//# sourceMappingURL=sync.js.map
