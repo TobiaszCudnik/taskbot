c96417b42be3b10e56aad62334b46ccb69d52bba
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-06-26 16:40:06 -0400

    removed old types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,4 @@
-/// <reference path="globals/lokijs/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
-/// <reference path="globals/underscore/index.d.ts" />
 /// <reference path="manual/googleapis/googleapis/googleapis.d.ts" />
 /// <reference path="modules/async/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
 /// <reference path="modules/uuid/index.d.ts" />
