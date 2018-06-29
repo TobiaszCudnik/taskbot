edbfa378d676fd13d74413e2abdac472295ba4fa
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-06-29 10:34:33 -0400

    fixed moment type errors cleanups

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,2 @@
-/// <reference path="globals/moment/index.d.ts" />
 /// <reference path="manual/googleapis/googleapis/googleapis.d.ts" />
-/// <reference path="modules/async/index.d.ts" />
 /// <reference path="modules/uuid/index.d.ts" />
