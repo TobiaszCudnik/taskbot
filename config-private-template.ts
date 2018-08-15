647d7f5f477b3253bde2480cfe891a1a4aabddce
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-08-15 00:14:10 -0400

    config templates

--- config-private-template.ts
+++ config-private-template.ts
@@ -1,8 +1,12 @@
 export default {
+  service: {
+    email: 'email@gmail.com'
+  },
   google: {
     // project credentials
     client_id: '',
     client_secret: '',
     redirect_url: ''
-  }
+  },
+  firebase_admin: {}
 }
